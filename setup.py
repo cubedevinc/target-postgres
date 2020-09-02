4b6cf59d107d4a847fd208e0cf98e5282329b3e0
Paul Harper <paul@pathlight.com>
2020-09-01 17:20:43 -0700

    version bump to 1.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="target-postgres",
-    version="1.1.6",
+    version="1.1.7",
     description="Singer.io target for Postgres",
     author="Statsbot",
     url="https://statsbot.co",
