46815d43b22541978ffa8cbc7e2771d381b2da37
vorpal56 <anthonysukadil1@gmail.com>
2022-03-31 11:42:27 -0400

    Bump to v1.1.21

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="target-postgres",
-    version="1.1.19",
+    version="1.1.21",
     description="Singer.io target for Postgres",
     author="Statsbot",
     url="https://statsbot.co",
