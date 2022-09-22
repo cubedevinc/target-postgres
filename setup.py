4392259a60970eae5f98403fd3d267a4a0071462
Vivian Qu <vivian@pathlight.com>
2022-09-22 11:08:41 -0700

    Bump version to v1.1.23

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="target-postgres",
-    version="1.1.22",
+    version="1.1.23",
     description="Singer.io target for Postgres",
     author="Statsbot",
     url="https://statsbot.co",
