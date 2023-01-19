56cc2881e4c11b2d9eb916ab9fc4dca88addc2ec
Paul Harper <benekastah@gmail.com>
2023-01-19 13:23:41 -0800

    v1.1.24

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="target-postgres",
-    version="1.1.23",
+    version="1.1.24",
     description="Singer.io target for Postgres",
     author="Statsbot",
     url="https://statsbot.co",
